ea85e25643ee7c037318f0038016d87942c12c74
lordbenedikt <66784958+lordbenedikt@users.noreply.github.com>
2021-08-12 00:40:12 +0200

    FPS

--- Dungeon_Game/Assets/Player.cs
+++ Dungeon_Game/Assets/Player.cs
@@ -2,30 +2,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
-{
-    private CharacterController _controller; 
-    [SerializeField]
-    public float moveSpeed = 5.0f;
+{ 
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Camera playerCamera;
+    public float lookSpeed = 16.0f;
+    public float lookXLimit = 45.0f;
 
-    // Start is called before the first frame update
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    [HideInInspector]
+    public bool canMove = true;
+
     void Start()
     {
-        _controller = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        int left = Input.GetKey(KeyCode.A) ? 1 : 0;
-        int right = Input.GetKey(KeyCode.D) ? 1 : 0;
-        int up = Input.GetKey(KeyCode.W) ? 1 : 0;
-        int down = Input.GetKey(KeyCode.S) ? 1 : 0;
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        Vector3 move = new Vector3(right - left, 0, up - down);
-        move = move.normalized * moveSpeed; 
-        Vector3 velocity = move;
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
 
-        _controller.Move(velocity * Time.deltaTime);
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Player and Camera rotation
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
     }
 }
+
+    // void Start()
+    // {
+    //     _controller = GetComponent<CharacterController>();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     int left = Input.GetKey(KeyCode.A) ? 1 : 0;
+    //     int right = Input.GetKey(KeyCode.D) ? 1 : 0;
+    //     int up = Input.GetKey(KeyCode.W) ? 1 : 0;
+    //     int down = Input.GetKey(KeyCode.S) ? 1 : 0;
+
+    //     Vector3 move = new Vector3(right - left, 0, up - down);
+    //     move = move.normalized * moveSpeed; 
+    //     Vector3 velocity = move;
+
+    //     _controller.Move(velocity * Time.deltaTime);
+    // }
