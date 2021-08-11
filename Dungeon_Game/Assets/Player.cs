ddf535331b853febe691050fa3c8f2fed149f628
lordbenedikt <66784958+lordbenedikt@users.noreply.github.com>
2021-08-12 00:17:28 +0200

    First Person Camera

--- Dungeon_Game/Assets/Player.cs
+++ Dungeon_Game/Assets/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     private CharacterController _controller; 
     [SerializeField]
-    private float _speed = 5.0f;
+    public float moveSpeed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +17,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Debug.Log(horizontalInput);
+        int left = Input.GetKey(KeyCode.A) ? 1 : 0;
+        int right = Input.GetKey(KeyCode.D) ? 1 : 0;
+        int up = Input.GetKey(KeyCode.W) ? 1 : 0;
+        int down = Input.GetKey(KeyCode.S) ? 1 : 0;
 
-        Vector3 move = new Vector3(horizontalInput, 0, verticalInput);
-        Vector3 velocity = move * _speed;
+        Vector3 move = new Vector3(right - left, 0, up - down);
+        move = move.normalized * moveSpeed; 
+        Vector3 velocity = move;
 
         _controller.Move(velocity * Time.deltaTime);
     }
