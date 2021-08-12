d25064ec59e4b6456fa2f8502a45f5d348b9109a
lordbenedikt <66784958+lordbenedikt@users.noreply.github.com>
2021-08-12 17:28:55 +0200

    Torches everywhere

--- Dungeon_Game/Assets/Torch_flicker.cs
+++ Dungeon_Game/Assets/Torch_flicker.cs
@@ -6,9 +6,15 @@ public class Torch_flicker : MonoBehaviour
     float targetIntensity;
     public float minnumber;
     public float maxnumber;
+    float speedchangevalue;
 
     bool increasing = true;
     float curIntensity;
+
+    void Start()
+    {
+        speedchangevalue = 0.3f;
+    }
 
     void Update()
     {
@@ -16,24 +22,26 @@ public class Torch_flicker : MonoBehaviour
         {
             if (curIntensity <= targetIntensity)
             {
-                curIntensity += 0.3f;
+                curIntensity += speedchangevalue;
                 GetComponent<Light>().intensity = curIntensity;
             }
             else
             {
                 SetTargetValue();
+                SetSpeedChangeValue();
             }
         }
         else
         {
             if (curIntensity >= targetIntensity)
             {
-                curIntensity -= 0.3f;
+                curIntensity -= speedchangevalue;
                 GetComponent<Light>().intensity = curIntensity;
             }
             else
             {
                 SetTargetValue();
+                SetSpeedChangeValue();
             }
         }
     }
@@ -51,5 +59,10 @@ public class Torch_flicker : MonoBehaviour
             increasing = true;
         }
         //rand_number = Mathf.Round(rand_number * 10.0f) / 10.0f;
+    }
+
+    void SetSpeedChangeValue()
+    {
+        speedchangevalue = Random.Range(0.1f, 0.5f);
     }
 }
