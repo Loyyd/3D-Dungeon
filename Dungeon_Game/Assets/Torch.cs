1ebc611eb2ba366fc7fadfda960aace31324c3cf
lordbenedikt <66784958+lordbenedikt@users.noreply.github.com>
2021-08-13 11:40:19 +0200

    Torches turn on

---
+++ Dungeon_Game/Assets/Torch.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject Light;
+    public GameObject Particle;
+    
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Player.transform.position, transform.position) <= 1f)
+        {
+            Light.SetActive(true);
+
+            Particle.SetActive(true);
+        }
+    }
+}
