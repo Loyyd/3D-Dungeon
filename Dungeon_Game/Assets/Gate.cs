0c66e0ba79890412d480e85208d1796133d6b8c1
lordbenedikt <66784958+lordbenedikt@users.noreply.github.com>
2021-12-13 19:11:27 +0100

    Level01 Done

--- Dungeon_Game/Assets/Gate.cs
+++ Dungeon_Game/Assets/Gate.cs
@@ -6,11 +6,14 @@ public class Gate : MonoBehaviour
 {
     public bool isOpen = true;
     public int group;
+    public GameObject child1;
+    public GameObject child2;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        anim = gameObject.GetComponent<Animator>();
     }
 
     public void Switch(bool open)
@@ -18,11 +21,16 @@ public class Gate : MonoBehaviour
         if(open)
         {
             isOpen = true;
-            gameObject.SetActive(true);
+            child1.GetComponent<BoxCollider>().enabled = true;
+            child2.GetComponent<BoxCollider>().enabled = true;
+            anim.Play("anim_close_open");
+
         } else
         {
             isOpen = false;
-            gameObject.SetActive(false);
+            child1.GetComponent<BoxCollider>().enabled = false;
+            child2.GetComponent<BoxCollider>().enabled = false;
+            anim.Play("anim_gate_close");
         }
     }
 
